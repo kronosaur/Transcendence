cf9304c5417dcfef13d350ada4be43da2aba0aad
George Moromisato <gpm@kronosaur.com>
2012-03-14 10:36:51 -0700

    Added CHexarcServiceStub.cpp

---
+++ Transcendence/TSUI/CHexarcServiceStub.cpp
@@ -0,0 +1,12 @@
+//	CHexarcService.cpp
+//
+//	CHexarcService class
+//	Copyright (c) 2011 by George Moromisato. All Rights Reserved.
+
+#include "stdafx.h"
+
+ICIService *CHexarcServiceFactory::Create (CHumanInterface &HI)
+	{
+	return NULL;
+	}
+
