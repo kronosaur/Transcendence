36df215d42bd0d5b9c688955ae8c7a1237d76690
George Moromisato <public@neurohack.com>
2015-07-08 13:18:52 -0700

    New icon and images

---
+++ Game/compileHD.bat
@@ -0,0 +1,2 @@
+@rem Compiles Human Space HD
+@transcompiler /input:"c:\Users\George\Documents\Elysium\Extensions\HumanSpaceHD\HumanSpaceHD.xml" /output:TDBs\StarsOfThePilgrimHD.tdb
--- Game/compileHSHD.bat
+++
@@ -1,2 +0,0 @@
-@rem Compiles Human Space HD
-@transcompiler /input:"c:\Users\George\Documents\Elysium\Extensions\HumanSpaceHD\HumanSpaceHD.xml" /output:TDBs\HumanSpaceHD.tdb
