22ee6ee15b43a84d0970b57b2c226a200692f692
George Moromisato <public@neurohack.com>
2015-07-10 14:53:28 -0700

    SteamRelease

---
+++ Game/compileSoundtrack.bat
@@ -0,0 +1,2 @@
+@rem Compiles SotP Soundtrack
+@transcompiler /input:"c:\Users\George\Documents\Elysium\Extensions\StarsOfThePilgrimSoundtrack\StarsOfThePilgrimSoundtrack.xml" /output:TDBs\StarsOfThePilgrimSoundtrack.tdb
