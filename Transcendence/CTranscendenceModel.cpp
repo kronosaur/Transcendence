5936616a67352b0c102a4653b97bd9608f0054d8
George Moromisato <public@neurohack.com>
2016-02-23 16:07:39 -0800

    No need for this debug info.

--- Transcendence/CTranscendenceModel.cpp
+++ Transcendence/CTranscendenceModel.cpp
@@ -2762,10 +2762,7 @@ void CTranscendenceModel::UseItem (CItem &Item)
 		//	Done
 
 		if (!sError.IsBlank())
-			{
 			pShip->SendMessage(NULL, sError);
-			::kernelDebugLogMessage(sError);
-			}
 
 		pShip->OnComponentChanged(comCargo);
 		}
