f86b51ef29d3ca9fc929ff648afff6c04439a0eb
George Moromisato <public@neurohack.com>
2016-11-10 20:44:02 -0800

    Batch file to compile Part II

---
+++ Game/compileP2.bat
@@ -0,0 +1,3 @@
+@rem Compiles Eternity Port
+@transcompiler /input:"c:\Users\GPM\Documents\Elysium\Extensions\VaultOfTheGalaxy\AncientRacesVol01\AncientRacesVol01.xml" /output:TDBs\AncientRacesVol01.tdb
+@transcompiler /input:"c:\Users\GPM\Documents\Elysium\Extensions\VaultOfTheGalaxy\VaultOfTheGalaxy\VaultOfTheGalaxy.xml" /entities:"TDBs\AncientRacesVol01.tdb" /output:TDBs\VaultOfTheGalaxy.tdb
