ed76348ef054c79f24df8dc729ed680aaaafb167
George Moromisato <public@neurohack.com>
2017-08-25 09:23:45 -0700

    Move CC and EP directories

--- Game/compileCC.bat
+++ Game/compileCC.bat
@@ -1,3 +1,3 @@
 @rem Compiles Corporate Command
-@transcompiler /input:"c:\Users\GPM\Documents\Elysium\Extensions\CorporateHierarchyVol01\CorporateHierarchyVol01.xml" /output:TDBs\CorporateHierarchyVol01.tdb
-@transcompiler /input:"c:\Users\GPM\Documents\Elysium\Extensions\CorporateCommand\CorporateExpansion.xml" /entities:"TDBs\CorporateHierarchyVol01.tdb" /output:TDBs\CorporateCommand.tdb
+@transcompiler /input:"c:\Users\GPM\Documents\Elysium\Extensions\CorporateCommand\CorporateHierarchyVol01\CorporateHierarchyVol01.xml" /output:TDBs\CorporateHierarchyVol01.tdb
+@transcompiler /input:"c:\Users\GPM\Documents\Elysium\Extensions\CorporateCommand\CorporateCommand\CorporateExpansion.xml" /entities:"TDBs\CorporateHierarchyVol01.tdb" /output:TDBs\CorporateCommand.tdb
