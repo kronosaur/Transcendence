02c0a11ee308a26f23bedc0e887b92a7899065a3
George Moromisato <public@neurohack.com>
2017-10-21 20:18:06 -0700

    Larger width

--- Game/imagesEP.bat
+++ Game/imagesEP.bat
@@ -1,1 +1,1 @@
-@transdata /adventure:0x900000 /typeImages /criteria:"st -scale:star; -scale:world; -extension:0x100000; -extension:0x830000" /xMargin:40 /width:1920
+@transdata /adventure:0x900000 /typeImages /criteria:"st -scale:star; -scale:world; -extension:0x100000; -extension:0x830000" /xMargin:40 /width:6880
--- Game/imagesP2.bat
+++ Game/imagesP2.bat
@@ -1,1 +1,2 @@
-@transdata /adventure:0x300000 /typeimages /criteria:"st -scale:star; -scale:world" /xMargin:40 /width:1920
+@transdata /adventure:0x300000 /typeimages /criteria:"st -scale:star; -scale:world" /xMargin:40 /width:6880
+
